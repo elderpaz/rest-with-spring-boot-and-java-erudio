a574c551755113028a9105621922ec5a18a19080
Elder Paz <eldercomercial@gmail.com>
2024-11-11 20:49:04 -0300

    First Steps in java with spring Boot 3

---
+++ 05_FistStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/elder/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.elder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
